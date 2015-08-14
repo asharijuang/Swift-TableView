0f3c667c65d78de3f6cfbf40cf5a5665346a53a6
asharijuang <asharijuang@me.com>
2015-08-14 16:53:46 +0700

    add table view delegate and datasource

--- Swift-TableView/ViewController.swift
+++ Swift-TableView/ViewController.swift
@@ -8,9 +8,10 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, UITableViewDelegate{
 
     @IBOutlet weak var slider: UISlider!
+    
     override func viewDidLoad() {
         super.viewDidLoad()
         // Do any additional setup after loading the view, typically from a nib.
@@ -23,6 +24,20 @@ class ViewController: UIViewController {
 
     @IBAction func sliderMove(sender: AnyObject) {
         print(slider.value)
+    }
+    
+    func tableView(tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        return 20
+    }
+    
+    
+    func tableView(tableView: UITableView, cellForRowAtIndexPath indexPath: NSIndexPath) -> UITableViewCell {
+        let cell = UITableViewCell(style: UITableViewCellStyle.Default, reuseIdentifier: "Cell")
+        
+        let timeTable = Int(slider.value * 20)
+        cell.textLabel?.text = String(timeTable * indexPath.row)
+        
+        return cell
     }
 
 }
